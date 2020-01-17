ae2c7ee668dfa760f2574493cbf4c8d90cd249e1
Timi Ogunkeye <timi.ogunkeye@gmail.com>
2020-01-17 21:41:23 +0100

    first commit

---
+++ src/test/java/Iceland/Tech/Test/iceland/IcelandApplicationTests.java
@@ -0,0 +1,13 @@
+package Iceland.Tech.Test.iceland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcelandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
