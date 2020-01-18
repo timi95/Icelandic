2e8242a15b7f0edf5b4f5c1ee1207727b01fb303
Timi Ogunkeye <timi.ogunkeye@gmail.com>
2020-01-18 20:17:51 +0100

    controller for store items

---
+++ src/main/java/Iceland/Tech/Test/iceland/controllers/StoreItemController.java
@@ -0,0 +1,57 @@
+package Iceland.Tech.Test.iceland.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import Iceland.Tech.Test.iceland.dto.StoreItemDTO;
+import Iceland.Tech.Test.iceland.responses.DefaultResponses;
+import Iceland.Tech.Test.iceland.services.StoreService;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+/**
+ * StoreItemController
+ */
+@RestController
+@RequestMapping("store")
+public class StoreItemController {
+
+    private static final Logger L = LogManager.getLogger(StoreItemController.class);
+
+    @Autowired
+    private StoreService storeService;
+    
+    // STOREITEMS
+    @GetMapping(path="item")
+    public ResponseEntity getStoreItems(@RequestParam(name = "page", defaultValue = "0") int page,
+                                        @RequestParam(name = "size", defaultValue = "10") int size ) {
+        try {
+            return ResponseEntity.ok(storeService.getItems( page,size ));
+        } catch (Exception e) {
+            L.error("Error during processing", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                   .body(DefaultResponses.response500("Error fetching users"+
+                    "  Error Case: "+e.getClass().getName()));            
+        }
+    }
+
+    @PostMapping(path="item")
+    public ResponseEntity createStoreItem(@RequestBody StoreItemDTO itemDTO) {
+        try {
+            return ResponseEntity.ok(storeService.createStoreItem( itemDTO ));
+        } catch (Exception e) {
+            L.error("Error during processing", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                   .body(DefaultResponses.response500("Error fetching users"+
+                    "  Error Case: "+e.getClass().getName()));            
+        }
+    }
+    
+}
