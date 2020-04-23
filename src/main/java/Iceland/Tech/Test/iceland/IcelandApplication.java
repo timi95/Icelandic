d9c07ca83f297dc650f079b9a85faa7e245e9b99
timi95 <timi.ogunkeye@gmail.com>
2020-04-23 15:34:34 +0100

    servlet extension removed
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- src/main/java/Iceland/Tech/Test/iceland/IcelandApplication.java
+++ src/main/java/Iceland/Tech/Test/iceland/IcelandApplication.java
@@ -5,21 +5,14 @@ import Iceland.Tech.Test.iceland.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableConfigurationProperties(StorageProperties.class)
-public class IcelandApplication extends SpringBootServletInitializer {
+public class IcelandApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IcelandApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(IcelandApplication.class);
 	}
 
 }
