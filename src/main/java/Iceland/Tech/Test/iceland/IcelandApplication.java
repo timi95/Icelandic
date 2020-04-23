2d0de13c81e2b455803789aebc8e3a66dd66f993
timi95 <timi.ogunkeye@gmail.com>
2020-04-23 15:23:32 +0100

    servlet extension for tomcat
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- src/main/java/Iceland/Tech/Test/iceland/IcelandApplication.java
+++ src/main/java/Iceland/Tech/Test/iceland/IcelandApplication.java
@@ -1,14 +1,25 @@
 package Iceland.Tech.Test.iceland;
 
+import Iceland.Tech.Test.iceland.StorageProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
-public class IcelandApplication {
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableConfigurationProperties(StorageProperties.class)
+public class IcelandApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IcelandApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(IcelandApplication.class);
 	}
 
 }
